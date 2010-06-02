d8dcebb7610c10d323a2a8435e886fb31c015bb1
goran <goran@1a30a1b9-0fc7-4f74-b692-49697e487d08>
2010-06-02 18:40:21 +0000

    Struktur
    git-svn-id: file:///home/node/svn/algodat/fahrplanauskunft@4 1a30a1b9-0fc7-4f74-b692-49697e487d08

--- fahrplanauskunft.c
+++ fahrplanauskunft.c
@@ -10,6 +10,8 @@ int main(int argc, char *argv[])
 	if(argc != 2)
 	{
 		printF1();
+		return EXIT_FAILURE;
 	}
+	
 	return EXIT_SUCCESS;
 }
--- fahrplanauskunft.h
+++ fahrplanauskunft.h
@@ -1,3 +1,15 @@
+
+// EINLESE-STRUCT
+struct subway
+{
+	char station[25];
+	int time;
+	
+	subway* nxt;
+	//subway* prev;
+};
+
+// USAGE-NACHRICHT
 void printF1()
 {
 	fprintf(stderr, "USAGE: ./fahrplanauskunft FILE\n");
