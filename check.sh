45686d0fadd6f408df21faf6fb80c1370dc00e4b
Alexander Kumbeiz <eeezyy@gmail.com>
2010-06-04 22:42:44 +0000

    added testfiles "empty", "errorTestcase1.txt"
    git-svn-id: file:///home/node/svn/algodat/fahrplanauskunft@22 1a30a1b9-0fc7-4f74-b692-49697e487d08

--- check.sh
+++ check.sh
@@ -9,6 +9,8 @@ ZWOPARAMETER="ubahn.txt empty"
 NONEXISTFILE=nonexistfile
 EMPTYFILE=empty
 EXAMPLEFILE=ubahn.txt
+# ErrorTestCases
+ETC1=errTestcase1.txt
 
 # always check after running program
 # 1. Parameter which is testet with the program, use quotemarks
@@ -29,10 +31,15 @@ checkTest () {
 }
 
 # Test cases
-# ------------
+# ------------	
 
+# FAILURE
 checkTest "${NOPARAMETER}" $EXIT_FAILURE "Test without parameter"
 checkTest "${NONEXISTFILE}" $EXIT_FAILURE "Test with nonexist file"
 checkTest "${ZWOPARAMETER}" $EXIT_FAILURE "Test with two files"
+checkTest "${ZWOPARAMETER}" $EXIT_FAILURE "Test with two files"
+checkTest "${ETC1}" $EXIT_FAILURE "Test with wrong format in file: ${ETC1}"
+
+#SUCCESS
 checkTest "${EMPTYFILE}" $EXIT_SUCCESS "Test with empty file"
 checkTest "${EXAMPLEFILE}" $EXIT_SUCCESS "Test with example file"
