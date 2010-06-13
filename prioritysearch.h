f7ec9594cdf73bfca3261ae86b5a1233422c904e
Alexander Kumbeiz <eeezyy@gmail.com>
2010-06-13 20:12:17 +0000

    some changes in search
    git-svn-id: file:///home/node/svn/algodat/fahrplanauskunft@69 1a30a1b9-0fc7-4f74-b692-49697e487d08

--- prioritysearch.h
+++ prioritysearch.h
@@ -1,24 +1,26 @@
 
 void search(station *startStation, station *endStation, heapnode **heap) {
-	station *currentStation = NULL;
+	station *currentStation = startStation;
 	while(currentStation != endStation) {
-		path *tempPath = startStation->p;
+		path *tempPath = currentStation->p;
 		station *tempStation = startStation;
 		while(tempPath != NULL) {
 			if(tempPath->halt->lengthSum > 0) {
 				fprintf(stdout, "lengthSum > 0\n");
 			}
+				fprintf(stdout, "test1\n");
 			tempPath->halt->lengthSum = startStation->lengthSum + tempPath->length;
 			heapNodeInsert(heap, tempPath->halt);
-			tempPath = tempPath->next;
 			if(tempPath->halt == endStation) {
 				fprintf(stdout, "endStation 2.\n");
 			}
+			tempPath = tempPath->next;
 		}
+				fprintf(stdout, "test2\n");
 		currentStation = heapNodeRemove(heap);
 		currentStation->prev = startStation;
 		fprintf(stdout, "%s\n", currentStation->name);
-		if(tempPath->halt == endStation) {
+		if(currentStation == endStation) {
 			fprintf(stdout, "endStation 2.\n");
 			return;
 		}
