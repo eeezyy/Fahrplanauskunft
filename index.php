a0b2c8b209f18cfd86e6a411c91c16bca7febb51
Alexander Kumbeiz <eeezyy@gmail.com>
2010-06-13 04:55:15 +0000

    PHP-Exec test
    git-svn-id: file:///home/node/svn/algodat/fahrplanauskunft@52 1a30a1b9-0fc7-4f74-b692-49697e487d08

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
 	echo "PHP-Test in SVN";
+	$command  = "echo -e "Startstation\nEndstation\n" | ./fahrplanauskunft ubahn.txt"
+	$output = new array();
+	$return_val;
+	$result = exec ($command, $output, $return_var);
+	echo "result: ".$result;
+	printv($output);
+	echo "return_var: ".$return_var;
 ?>
