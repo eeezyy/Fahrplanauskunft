2bd5a5b72ca4594099c4d9587a164f772f348890
Alexander Kumbeiz <eeezyy@gmail.com>
2010-06-14 19:25:55 +0000

    testcases
    git-svn-id: file:///home/node/svn/algodat/fahrplanauskunft@78 1a30a1b9-0fc7-4f74-b692-49697e487d08

--- check.sh
+++ check.sh
@@ -14,12 +14,13 @@ ETC1=errTestcase1.txt
 
 # always check after running program
 # 1. Parameter which is testet with the program, use quotemarks
-# 1. Expected return value
-# 2. parameter is test description
+# 2. Expected return value
+# 3. parameter is test description
+# 4. Input for startStation and endStation "\"startStation\"\n\"endStation\"\n"
 checkTest () {
 	COUNTTEST=`expr $COUNTTEST + 1`
 	echo "$COUNTTEST. Test: $3"
-	./${PROGRAM} $1 > /dev/null
+	echo -e $4 | ./${PROGRAM} $1 1> /dev/null
 	if test $? -eq $2
 	then
 		echo "Successful"
@@ -42,6 +43,5 @@ checkTest "${ETC1}" $EXIT_FAILURE "Test with wrong format in file: ${ETC1}"
 
 #SUCCESS
 checkTest "${EMPTYFILE}" $EXIT_SUCCESS "Test with empty file"
-checkTest "${EXAMPLEFILE}" $EXIT_SUCCESS "Test with example file"
-checkTest "test1.txt" $EXIT_SUCCESS "Test with one line without newLine at the end"
+checkTest "test1.txt" $EXIT_SUCCESS "Test with one line without newLine at the end" "test\nbla\n"
 
