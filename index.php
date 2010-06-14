0a2672a4caabcecee3501ce38c8048cd99e05180
Alexander Kumbeiz <eeezyy@gmail.com>
2010-06-14 20:24:34 +0000

    some changes to webinterface
    git-svn-id: file:///home/node/svn/algodat/fahrplanauskunft@81 1a30a1b9-0fc7-4f74-b692-49697e487d08

--- index.php
+++ index.php
@@ -1,12 +1,34 @@
+<head>
+	<title>Fahrplanauskunft</title>
+</head>
+<body>
+	<h1>Fahrplanauskunft U-Bahn Wien</h1>
+	<form action='<?php echo $_SERVER["PHP_SELF"]; ?>' method='GET'>
+		<table>
+			<tr>
+				<td>Aktuelle Station:</td><td><input type='text' name='start' /></td>
+			</tr>
+			<tr>
+				<td>Ziel-Station:</td><td><input type='text' name='end' /></td>
+			</tr>
+			<tr>
+				<td /><td><input type='submit'/></td>
+			</tr>
+		</table>
+	</form>
+</body>
+
 <?php
-	echo "PHP-Test in SVN<br />\n";
-	$command  = "echo -e \"Startstation\\nEndstation\\n\" | ./fahrplanauskunft ubahn.txt";
-	$output = array();
-	$return_val;
-	$result = exec ($command, $output, $return_var);
-	echo "result: ".$result."<br />\n";
-	foreach($output as $line) {
-		echo $line."<br />\n";
+	if(isset($_GET['start']) && isset($_GET['end'])) {
+		$command  = "echo -e \"".$_GET['start']."\\n".$_GET['end']."\\n\" | ./fahrplanauskunft ubahn.txt";
+		$output = array();
+		$return_val;
+		$result = exec ($command, $output, $return_var);
+		//echo "result: ".$result."<br />\n";
+		if($return_var = 0) {
+			foreach($output as $line) {
+				echo $line."<br />\n";
+			}
+		}
 	}
-	echo "return_var: ".$return_var;
 ?>
